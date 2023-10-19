9b0d767889c62c97dd0a4d83132190309a9be431
maria jose Lizasoain <mariajoselizasoain@marias-MacBook-Pro.local>
2023-10-18 23:48:37 -0300

    segunda preentrega

--- app.js
+++ app.js
@@ -1,55 +1,72 @@
-let formulario = {};
+// Defino un objeto formulario para almacenar los datos de la reserva.
+const formulario = {
+    nombre: '',
+    apellido: '',
+    edad: 0,
+    participantes: 0,
+    horario: '',
+    cancha: '',
+    turnos: 0,
+    pago: '',
+    correo: '',
+    // Defino un método para mostrar los datos de la reserva.
+    reserva: function () {
+        const apellidonombre = this.nombre + ", " + this.apellido;
+        console.log("La reserva se realizo a nombre de " + apellidonombre + " para el deporte " + this.cancha);
+    }
+};
 
-// Se solicita y muestra la edad del usuario.
-formulario.edad = prompt("Ingrese su edad:");
+// Pido la edad al usuario y la almaceno en el objeto formulario.
+formulario.edad = parseInt(prompt("Ingrese su edad:"));
 console.log(`Edad: ${formulario.edad}`);
 
-// Se convierte la edad en un número.
-const edadNumero = parseInt(formulario.edad);
-
-// Se verifica si la edad no es un número válido o si es menor de 18 años.
-if (isNaN(edadNumero) || edadNumero < 18) {
+// Verifico si la edad ingresada es válida y si el usuario es mayor de 18 años.
+if (isNaN(formulario.edad) || formulario.edad < 18) {
     alert("El usuario es menor de 18 años o ha proporcionado una entrada no válida.");
 } else {
-    // Continúa con la recopilación de datos.
+    // Continúo pidiendo el resto de los datos para la reserva.
     formulario.nombre = prompt("Ingrese su nombre:");
     console.log(`Nombre: ${formulario.nombre}`);
 
-    // Se solicita y muestra el apellido.
     formulario.apellido = prompt("Ingrese su apellido:");
     console.log(`Apellido: ${formulario.apellido}`);
 
-    // Se solicita y muestra la cantidad de participantes.
-    formulario.participantes = prompt("Ingrese la cantidad de participantes:");
+    // Pido y almaceno la cantidad de participantes.
+    formulario.participantes = parseInt(prompt("Ingrese la cantidad de participantes:"));
     console.log(`Cantidad de participantes: ${formulario.participantes}`);
 
-    // Se solicita y muestra el horario deseado con opciones.
+    // Pido el horario deseado y lo almaceno en el objeto formulario.
     const opcionesHorario = ["Mañana", "Tarde", "Noche"];
-    const indiceHorario = prompt("Elija el horario deseado:\n" + opcionesHorario.join("\n"));
-    formulario.horario = opcionesHorario[indiceHorario];
+    const respuestaHorario = prompt("Elija el horario deseado:\n" + opcionesHorario.join("\n")).toUpperCase();
+    formulario.horario = respuestaHorario;
     console.log(`Horario deseado: ${formulario.horario}`);
 
-    // Se solicita y muestra cuántos turnos desean usar.
-    formulario.turnos = prompt("Ingrese cuántos turnos desean usar:");
+    // Pido y almaceno la cantidad de turnos que desean usar.
+    formulario.turnos = parseInt(prompt("Ingrese cuántos turnos desean usar:"));
     console.log(`Cantidad de turnos: ${formulario.turnos}`);
 
-    // Se solicita y muestra si pagan en efectivo con opciones.
-    const opcionesPago = ["Sí", "No"];
-    const indicePago = prompt("¿Pagan en efectivo?\n" + opcionesPago.join("\n"));
-    formulario.efectivo = opcionesPago[indicePago] === "Sí";
-    console.log(`Pago en efectivo: ${formulario.efectivo ? "Sí" : "No"}`);
+    // Pido la forma de pago y la almaceno en el objeto formulario.
+    const opcionesPago = ["Efectivo", "Tarjeta de credito", "APPS", "Transferencias"];
+    const respuestaPago = prompt("¿Como abonan?\n" + opcionesPago.join("\n")).toUpperCase();
+    formulario.pago = respuestaPago;
+    console.log(`Pago con: ${formulario.pago}`);
 
-    // Se solicita y muestra la cancha deseada.
-    formulario.cancha = prompt("Ingrese la cancha deseada:");
-    console.log(`Cancha deseada: ${formulario.cancha}`);
+    // Pido la cancha deseada y la almaceno en el objeto formulario.
+    const opcionesCanchas = ["Voley", "Futbol", "Tenis", "Basquet", "Padel"];
+    const respuestaCanchas = prompt("Elija la cancha deseada:\n" + opcionesCanchas.join("\n"));
+    formulario.cancha = respuestaCanchas;
+    console.log(`Deporte deseado: ${formulario.cancha}`);
 
-    // Se solicita y muestra el correo de reserva.
+    // Pido el correo de reserva y lo almaceno en el objeto formulario.
     formulario.correo = prompt("Ingrese su correo de reserva:");
     console.log(`Correo de reserva: ${formulario.correo}`);
 
-    // Se muestra la confirmación de que los datos del formulario se han completado.
+    // Muestro una confirmación de que los datos del formulario se han completado.
     console.log("Datos del formulario completados:");
     for (let clave in formulario) {
         console.log(`${clave}: ${formulario[clave]}`);
     }
 }
+
+// Llamo al método reserva para mostrar los datos de la reserva.
+formulario.reserva();
